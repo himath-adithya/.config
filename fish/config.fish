21330974d2ec94947f1edeff864899c8da425b92
himath-adithya <himathadithya@gmail.com>
2024-11-05 01:58:36 +0530

    minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-set fish_greeting
-source (/usr/sbin/starship init fish --print-full-init | psub)
-#starship init fish | source
-#BLK-CHR-DIR-EXE-REG-HARDLINK-SYMLINK-MISSING-ORPHAN-FIFO-SOCK-OTHER
-export NNN_FCOLORS='C1E26d95006650F7C6D6ABC4'
-# export VISUAL=/usr/bin/nvim
-export VISUAL=/usr/bin/helix
-export COLORTERM=truecolor
+    set fish_greeting
+    source (/usr/sbin/starship init fish --print-full-init | psub)
+    #starship init fish | source
+    #BLK-CHR-DIR-EXE-REG-HARDLINK-SYMLINK-MISSING-ORPHAN-FIFO-SOCK-OTHER
+    export NNN_FCOLORS='C1E26d95006650F7C6D6ABC4'
+    # export VISUAL=/usr/bin/nvim
+    export VISUAL=/usr/bin/helix
+    export COLORTERM=truecolor
+    export DEBUGINFOD_URLS="https://debuginfod.archlinux.org"
 end
